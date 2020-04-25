018af5789edba97eba4ee1e237628fac4327b561
jixin <2313037339@qq.com>
2020-04-25 19:54:59 +0800

    测试我想要的功能

--- src/main/java/com/orbyun/Hello.java
+++ src/main/java/com/orbyun/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
         System.out.println("dev分支456");
         System.out.println("dev分支123");
         System.out.println("增加一个dev");
+        System.out.println("测试我想要的功能");
     }
 }
