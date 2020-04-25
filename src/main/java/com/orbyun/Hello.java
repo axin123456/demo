3c8b89ef70218ce63ce8c7b9b19070b28b7fe549
axin123456 <53326380+axin123456@users.noreply.github.com>
2020-04-25 19:58:35 +0800

    Update Hello.java

--- src/main/java/com/orbyun/Hello.java
+++ src/main/java/com/orbyun/Hello.java
@@ -4,8 +4,6 @@ public class Hello {
 
 
     public static void main(String[] args) {
-        System.out.println("dev分支456");
-        System.out.println("dev分支123");
         System.out.println("增加一个dev");
         System.out.println("测试我想要的功能");
     }
